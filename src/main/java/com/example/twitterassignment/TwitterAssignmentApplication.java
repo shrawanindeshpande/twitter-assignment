36f7f9c7194375c89f1e4039e204059e4b0be0a2
Shrawani Deshpande <70814083+shrawanindeshpande@users.noreply.github.com>
2024-05-04 18:21:21 +0530

    first commit

---
+++ src/main/java/com/example/twitterassignment/TwitterAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twitterassignment/TwitterAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
